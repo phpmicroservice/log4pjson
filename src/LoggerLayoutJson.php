b540b88a1fae048822c7497b88059b52ba526057
dongasai <1514582970@qq.com>
2019-12-15 22:19:23 +0800

    init

---
+++ src/LoggerLayoutJson.php
@@ -0,0 +1,43 @@
+<?php
+
+class LoggerLayoutJson extends \LoggerLayout
+{
+
+    /** Whether to include the event's location information (slow). */
+    protected $locationInfo = false;
+
+    /** Sets the location information flag. */
+    public function setLocationInfo($value)
+    {
+        $this->setBoolean('locationInfo', $value);
+    }
+
+    /** Returns the location information flag. */
+    public function getLocationInfo()
+    {
+        return $this->locationInfo;
+    }
+
+    public function format(\LoggerLoggingEvent $event)
+    {
+        // If required, initialize the location data
+        if ($this->locationInfo) {
+            $event->getLocationInformation();
+        }
+
+        $info = [
+            'fqcn' => $event->getFullQualifiedClassname(),
+            'level' => $event->getLevel(),
+            'ndc' => $event->getNDC(),
+            'message' => $event->getMessage(),
+            'sname' => SERVICE_NAME,
+            'addr' => APP_HOST_IP,
+            'threadName' => $event->getThreadName(),
+            'timeStamp' => $event->getTimeStamp(),
+        ];
+        if ($this->locationInfo) {
+            $info['locationInfo'] = $event->getLocationInformation();
+        }
+        return json_encode($info) . PHP_EOL;
+    }
+}
